f27a9a80ebb9c6bf3e83ce54af3c2c04100737ca
Rishabh Bajaj <rishabhbajajnitr@gmail.com>
2025-05-09 15:20:55 +0900

    updated files to work with multiple datasets

--- global_reg.py
+++ global_reg.py
@@ -1,9 +1,18 @@
+import os
+
 import open3d.examples.visualization.draw
 
 from utils import *
 
-base_dir = "/media/rishabh/SSD_1/Data/Blender_Renders/ct_scan_foot"
-output_dir = osp.join(base_dir, "project_2_images")
+# base_dir = "/media/rishabh/SSD_1/Data/Blender_Renders/ct_scan_foot"
+# output_name = "project_2_images"
+
+base_dir = "/home/rishabh/projects/gaussian-splatting/output/bunny_v3/point_cloud/iteration_30000"
+output_name = "blender_tf"
+
+output_dir = osp.join(base_dir, output_name)
+
+os.makedirs(output_dir, exist_ok=True)
 
 # scaled_M2_path = osp.join(output_dir, "scaled_M2.ply")
 # scaled_M2 = o3d.io.read_point_cloud(scaled_M2_path)
@@ -73,11 +82,25 @@ output_dir = osp.join(base_dir, "project_2_images")
 # result_T_m1_m2 = transformation_matrix @ np.linalg.inv(ext_mat2)
 
 # obtained from manually aligning the 2pcd using point picking in CloudCompare
-result_T_m2_m1 = np.array([[ 0.03281417, -0.47197083, -0.08237366, -0.43157973],
-       [-0.02093014, -0.08389998,  0.47237842,  0.16128158],
-       [-0.4786479 , -0.02868767, -0.0263032 , -0.22229815],
-       [ 0.        ,  0.        ,  0.        ,  1.        ]]) # THIS TF matrix from cloud compare contains scaling factor too (0.480228)
+# result_T_m2_m1 = np.array([[ 0.03281417, -0.47197083, -0.08237366, -0.43157973],
+#        [-0.02093014, -0.08389998,  0.47237842,  0.16128158],
+#        [-0.4786479 , -0.02868767, -0.0263032 , -0.22229815],
+#        [ 0.        ,  0.        ,  0.        ,  1.        ]]) # THIS TF matrix from cloud compare contains scaling factor too (0.480228)
 
+# obtained from manually aligning the 2pcd using point picking in CloudCompare
+if "foot" in base_dir:
+    result_T_m2_m1 = np.array([
+        [ 0.03281417, -0.47197083, -0.08237366, -0.43157973],
+        [-0.02093014, -0.08389998,  0.47237842,  0.16128158],
+        [-0.4786479 , -0.02868767, -0.0263032 , -0.22229815],
+        [ 0.        ,  0.        ,  0.        ,  1.        ]]) # THIS TF matrix from cloud compare contains scaling factor too
+elif "bunny" in base_dir:
+    result_T_m2_m1 = np.array([
+        [-0.005651696119, -0.009304529987, -0.152641370893,  0.026200454682],
+        [ 0.152924671769, -0.000424805272, -0.005636291113,  0.040806733072],
+        [-0.000081029211, -0.152745380998,  0.009313870221,  0.052693203092],
+        [ 0.0,             0.0,             0.0,             1.0]# THIS TF matrix from cloud compare contains scaling factor too (0.480228)
+])
 
 rot = R.from_matrix(result_T_m2_m1[:3,:3])
 # rot_transformed = np.eye(4)
@@ -87,7 +110,8 @@ rot = R.from_matrix(result_T_m2_m1[:3,:3])
 
 euler_angles = rot.as_euler('zyx', degrees=False)
 
-test_M2 = o3d.io.read_point_cloud('/home/rishabh/projects/r2_gaussian/output/foot/point_cloud/iteration_30000/density_test.ply')
+# test_M2 = o3d.io.read_point_cloud('/home/rishabh/projects/r2_gaussian/output/foot/point_cloud/iteration_30000/density_test.ply')
+test_M2 = o3d.io.read_point_cloud("/home/rishabh/projects/gaussian-splatting/output/bunny_v3/point_cloud/iteration_30000/cleaned_point_cloud.ply")
 test_M2.transform(result_T_m2_m1)
 o3d.io.write_point_cloud(osp.join(output_dir, "scaled_M2_transformed.ply"), test_M2)
 
