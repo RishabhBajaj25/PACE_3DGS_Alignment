c337c86cba250f743a7d19770eaf066d4cb67b03
RishabhBajaj25 <rishabhbajajnitr@gmail.com>
2026-01-15 17:05:36 -0500

    refactored to work with leica and 3dgs

--- global_reg.py
+++ global_reg.py
@@ -1,48 +1,49 @@
+import numpy as np
+from scipy.spatial.transform import Rotation as R_scipy
+import open3d as o3d
+import os.path as osp
 import os
+import csv
 
-import open3d.examples.visualization.draw
-
-from utils import *
-
-# base_dir = "/media/rishabh/SSD_1/Data/Blender_Renders/ct_scan_foot"
-# output_name = "project_2_images"
-
-base_dir = "/home/rishabh/projects/gaussian-splatting/output/bunny_v3/point_cloud/iteration_30000"
-output_name = "blender_tf"
-
+base_dir = "/home/pace-ubuntu/datasets/leica/EAST/registration"
+output_name = "rough"
 output_dir = osp.join(base_dir, output_name)
-
 os.makedirs(output_dir, exist_ok=True)
 
-
-# obtained from manually aligning the 2pcd using point picking in CloudCompare
-if "foot" in base_dir:
-    result_T_m2_m1 = np.array([
-        [ 0.03281417, -0.47197083, -0.08237366, -0.43157973],
-        [-0.02093014, -0.08389998,  0.47237842,  0.16128158],
-        [-0.4786479 , -0.02868767, -0.0263032 , -0.22229815],
-        [ 0.        ,  0.        ,  0.        ,  1.        ]]) # THIS TF matrix from cloud compare contains scaling factor too
-elif "bunny" in base_dir:
-    result_T_m2_m1 = np.array([
-        [-0.005651696119, -0.009304529987, -0.152641370893,  0.026200454682],
-        [ 0.152924671769, -0.000424805272, -0.005636291113,  0.040806733072],
-        [-0.000081029211, -0.152745380998,  0.009313870221,  0.052693203092],
-        [ 0.0,             0.0,             0.0,             1.0]# THIS TF matrix from cloud compare contains scaling factor too (0.480228)
+result_T_m2_m1 = np.array([
+    [-1.548305869102, 0.002351583214, -0.016247715801, 1787.160034179688],
+    [0.016255933791, 0.005465727299, -1.548297882080, 2714.197021484375],
+    [-0.002294085687, -1.548381447792, -0.005490107927, 177.568634033203],
+    [0.000000000000, 0.000000000000, 0.000000000000, 1.000000000000]
 ])
 
-rot = R.from_matrix(result_T_m2_m1[:3,:3])
+R_s = result_T_m2_m1[:3, :3]
 
+# Compute scale as the mean of the column norms
+scale = np.mean([np.linalg.norm(R_s[:,0]),
+                 np.linalg.norm(R_s[:,1]),
+                 np.linalg.norm(R_s[:,2])])
+print("Scale factor:", scale)
 
-euler_angles = rot.as_euler('zyx', degrees=False)
+R = R_s / scale
+
+rot = R_scipy.from_matrix(R)
+euler_angles = rot.as_euler('zyx', degrees=True)  # or degrees=False
+print("Euler angles (zyx):", euler_angles)
+
+t = result_T_m2_m1[:3, 3]
+print("Translation:", t)
 
 # test_M2 = o3d.io.read_point_cloud('/home/rishabh/projects/r2_gaussian/output/foot/point_cloud/iteration_30000/density_test.ply')
-test_M2 = o3d.io.read_point_cloud("/home/rishabh/projects/gaussian-splatting/output/bunny_v3/point_cloud/iteration_30000/cleaned_point_cloud.ply")
-test_M2.transform(result_T_m2_m1)
-o3d.io.write_point_cloud(osp.join(output_dir, "scaled_M2_transformed.ply"), test_M2)
+test_M2 = o3d.io.read_point_cloud("/home/pace-ubuntu/datasets/leica/EAST/registration/supersplat_export_50000 - Cloud.ply")
 
-# The TF matrix from cloud compare contains scaling factor too (yes, but still need to store it (to apply to gaussians later and not point cloud))
-# s =0.480228
-scale = np.loadtxt(osp.join(output_dir, "scale.txt"))
+test_M2.scale(scale, center = (0,0,0))
+test_M2.rotate(R, center = (0,0,0))
+test_M2.translate(t)
+
+o3d.io.write_point_cloud(osp.join(output_dir, "transformed_m2.ply"), test_M2)
+
+
 
 # Open the CSV file for writing
 with open(osp.join(output_dir, "global_reg_result.csv"), mode='w', newline='') as file:
