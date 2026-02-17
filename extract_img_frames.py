54d40857ff47cec34ea4bb5ec7024f7b90028293
RishabhBajaj25 <rishabhbajajnitr@gmail.com>
2026-02-17 13:01:23 -0500

    deleted scratch files

--- extract_img_frames.py
+++ extract_img_frames.py
@@ -1,37 +1,46 @@
 import cv2
 import os
 # Path to the video file
-video_path = '/home/pace-ubuntu/datasets/warehouse/IMG_6991.MOV'
-# Directory to save the frames
-every_nth_frame = 7
 
-save_dir = os.path.join(os.path.dirname(video_path), os.path.splitext(os.path.basename(video_path))[0] + '_frames_' + str(every_nth_frame) + '_fps')
-os.makedirs(save_dir, exist_ok=True)
+vid_paths = [
+    # '/home/rishabh/datasets/warehouse/insta360/VID_20260130_185446_00_001.mp4',
+    # '/media/rishabh/HDD_Windows/Insta_Datasets/Brewery/VID_20260213_134819_00_001.mp4',
+    # '/media/rishabh/HDD_Windows/Insta_Datasets/Brewery/VID_20260213_135000_00_002.mp4',
+    # '/media/rishabh/HDD_Windows/Insta_Datasets/Brewery/VID_20260213_135046_00_003.mp4',
+    '/media/rishabh/HDD_Windows/Insta_Datasets/Brewery/VID_20260213_135214_00_004.mp4']
 
-# Open the video file
-cap = cv2.VideoCapture(video_path)
+for video_path in vid_paths:
+    # video_path = '/home/rishabh/datasets/warehouse/insta360/VID_20260130_185446_00_001.mp4'
+    # Directory to save the frames
+    every_nth_frame = 2
 
-# Read the video frame by frame
-frame_count = 0
-saved_frame_count = 0  # This counts the actual saved frames
+    save_dir = os.path.join(os.path.dirname(video_path), os.path.splitext(os.path.basename(video_path))[0] + '_frames_' + str(every_nth_frame) + '_fps')
+    os.makedirs(save_dir, exist_ok=True)
 
-while cap.isOpened():
-    ret, frame = cap.read()
-    if not ret:
-        break
+    # Open the video file
+    cap = cv2.VideoCapture(video_path)
 
-    # Process every nth frame
-    if frame_count % every_nth_frame == 0:
-        # Rotate frame by 90 degrees
-        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    # Read the video frame by frame
+    frame_count = 0
+    saved_frame_count = 0  # This counts the actual saved frames
 
-        # Save the frame as an image file
-        frame_path = os.path.join(save_dir, f'frame_{saved_frame_count:04d}.jpg')
-        cv2.imwrite(frame_path, frame)
-        saved_frame_count += 1  # Increment only when a frame is saved
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
 
-    frame_count += 1
+        # Process every nth frame
+        if frame_count % every_nth_frame == 0:
+            # Rotate frame by 90 degrees
+            # frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
 
-# Release the video capture object
-cap.release()
-print(f'Extracted {saved_frame_count} frames from the video.')
+            # Save the frame as an image file
+            frame_path = os.path.join(save_dir, f'frame_{saved_frame_count:04d}.jpg')
+            cv2.imwrite(frame_path, frame)
+            saved_frame_count += 1  # Increment only when a frame is saved
+
+        frame_count += 1
+
+    # Release the video capture object
+    cap.release()
+    print(f'Extracted {saved_frame_count} frames from the video.')
